51463ea6df55dc7d16f506a5a99f00ec3fad2685
Fernando Kiosa <fernandojoaokiosa90@gmail.com>
2021-10-18 19:21:13 -0300

    adicionado componente professores e a sua rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { CadastrarAlunoComponent } from './alunos/cadastrar-aluno/cadastrar-alun
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AlunosComponent } from './alunos/alunos.component';
+import { ProfessoresComponent } from './professores/professores.component';
 
 const routes: Routes = [
   { path: 'alunos', component: AlunosComponent },
-  { path: 'cadastrar-aluno', component: CadastrarAlunoComponent}
+  { path: 'cadastrar-aluno', component: CadastrarAlunoComponent},
+  { path: 'professores', component: ProfessoresComponent}
+  
 ];
 
 @NgModule({
